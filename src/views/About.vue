231a5b07ca0319f23c2aa1e03b0f05efd559b12a
GP Buffo <gpbuffo@gmail.com>
2020-03-08 21:11:31 +0200

    Add Vuetensils component framework

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <VAlert>Hey, I'm an alert!</VAlert>
   </div>
 </template>
+
+<script>
+import { VAlert } from 'vuetensils';
+
+export default {
+  components: {
+    VAlert,
+  },
+};
+</script>
