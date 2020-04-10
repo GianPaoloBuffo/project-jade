9055aa3a376510afa67c82d4e709e7aadfdde740
GP Buffo <gpbuffo@gmail.com>
2020-04-10 10:21:27 +0200

    Remove Vuetensils component framework

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <VAlert>Hey, I'm an alert!</VAlert>
   </div>
 </template>
 
 <script>
-import { VAlert } from 'vuetensils';
-
 export default {
-  components: {
-    VAlert,
-  },
 };
 </script>
